2e52aacf3c1785499a3b8f1654c6811ffde77c11
mrandolph83 <mrandolph83@gmail.com>
2021-08-02 15:28:48 -0400

    Brainstormer user form functionality, eent listener on submit event

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
+
+
+const endPoint = "http://127.0.0.1:3000/api/v1/ideas"
 document.addEventListener('DOMContentLoaded', () => {
-   loadSite() 
+   
+   getIdeas()
+
+   // E.L. for user inputting data into Brainstormer form
+   const userBrainstormerForm = document.querySelector("#brainstormer-user-edit")
+   userBrainstormerForm.addEventListener("submit", (e) => 
+   createFormHandler(e))
 })
 
-    function loadSite(){
-    fetch("http://127.0.0.1:3000/api/v1/ideas")
-    .then(response => response.json())
-    .then(ideas => {
-       console.log(ideas)
-    })
-    }
+// Fetch/get data from API
+ function getIdeas() {
+   fetch(endPoint)
+     .then(res => res.json())
+     .then(ideas => {
+      //  debugger
+      ideas.data.forEach(idea => {
+         const ideaMarkup = `
+           <div data-id=${idea.id}>
+             <h3>${idea.attributes.character}</h3>
+             <h3>${idea.attributes.setup}</h3>
+             <h3>${idea.attributes.twist}</h3>
+           </div>
+           <br><br>`;
+ 
+           document.querySelector('#idea-container').innerHTML += ideaMarkup
+       })
+     })
+ }
+
+//  Brainstormer form, making variables/values upon "submit"
+ function createFormHandler(e) {
+    e.preventDefault()
+    const characterInput = document.querySelector("#character").value
+    const setupInput = document.querySelector("#setup").value
+    const twistInput = document.querySelector("#twist").value
+    
+ }
