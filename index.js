4f266157b2095b9cb684157a60fea52401d81ad3
mrandolph83 <mrandolph83@gmail.com>
2021-08-03 12:14:22 -0400

    Post request from form complete, add genre?

--- index.js
+++ index.js
@@ -34,12 +34,44 @@ document.addEventListener('DOMContentLoaded', () => {
 //  Brainstormer form, making variables/values upon "submit"
  function createFormHandler(e) {
     e.preventDefault()
+   //  Do I want these in the global scope? try innerHTML within 
+   // this function
     const characterInput = document.querySelector("#character").value
     const setupInput = document.querySelector("#setup").value
     const twistInput = document.querySelector("#twist").value
-   postFetch(characterInput, setupInput, twistInput)
+   postIdea(characterInput, setupInput, twistInput)
    }
 
-    function postFetch(characterInput, setupInput, twistInput) {
-      console.log(characterInput, setupInput, twistInput)
+
+    // Sending Post request to your API (backend create route)
+    function postIdea(character, setup, twist) {
+      // confirm these values are coming through properly
+      console.log(character, setup, twist);
+      // build body object
+      let bodyData = {character, setup, twist}
+    
+      fetch(endPoint, {
+        // POST request
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(bodyData)
+      })
+      
+      .then(response => response.json())
+      .then(idea => {
+        console.log(idea);
+        debugger
+        // const ideaData = idea.data
+        // // render JSON response
+        // const ideaMarkup = `
+        // <div data-id=${idea.id}>
+        //   <img src=${ideaData.attributes.image_url} height="200" width="250">
+        //   <h3>${ideaData.attributes.title}</h3>
+        //   <p>${ideaData.attributes.category.name}</p>
+        //   <button data-id=${ideaData.id}>edit</button>
+        // </div>
+        // <br><br>`;
+    
+        document.querySelector('#idea-container').innerHTML += ideaMarkup;
+      })
     }
